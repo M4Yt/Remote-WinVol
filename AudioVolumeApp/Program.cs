7a0f17dc97c488bc091954f53a603a2497a4b3c5
Fabian Thiellier (2source4) <fabian.thiellier@2source4.com>
2018-03-23 11:44:58 +0100

    prepared backend for media key functionality

--- AudioVolumeApp/Program.cs
+++ AudioVolumeApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AudioSwitcher.AudioApi;
 using AudioSwitcher.AudioApi.CoreAudio;
 using Fleck;
+using WindowsInput;
 
 namespace AudioVolumeApp
 {
@@ -17,6 +18,7 @@ namespace AudioVolumeApp
         static void Main(string[] args)
         {
             var observer = new VolumeObserver();
+            var mkh = new MediaKeyHandler();
             Master.VolumeChanged.Subscribe(observer);
 
             Server.Start(socket =>
@@ -32,8 +34,42 @@ namespace AudioVolumeApp
                     Console.WriteLine("Close!");
                     AllSockets.Remove(socket);
                 };
-                //socket.OnMessage = message => socket.Send(message);
-                socket.OnMessage = message => SetVolume(Double.Parse(message));
+                socket.OnMessage = message =>
+                {
+                    if (Double.TryParse(message, out double newVol))
+                    {
+                        SetVolume(newVol);
+                    } else
+                    {
+                        switch (message)
+                        {
+                            case "playpause":
+                                {
+                                    mkh.PlayPause();
+                                    break;
+                                }
+                            case "stop":
+                                {
+                                    mkh.Stop();
+                                    break;
+                                }
+                            case "prev":
+                                {
+                                    mkh.PreviousTrack();
+                                    break;
+                                }
+                            case "next":
+                                {
+                                    mkh.NextTrack();
+                                    break;
+                                }
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                    }
+                };
             });
 
             while (true)
@@ -79,6 +115,35 @@ namespace AudioVolumeApp
             {
                 socket.Send(Program.GetVolume().ToString());
             }
+        }
+    }
+
+    class MediaKeyHandler
+    {
+        private InputSimulator s = new InputSimulator();
+
+        public void PlayPause()
+        {
+            s.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PLAY_PAUSE);
+            Console.WriteLine("PlayPause Pressed");
+        }
+
+        public void Stop()
+        {
+            s.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_STOP);
+            Console.WriteLine("Stop Pressed");
+        }
+
+        public void PreviousTrack()
+        {
+            s.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PREV_TRACK);
+            Console.WriteLine("PrevTrack Pressed");
+        }
+
+        public void NextTrack()
+        {
+            s.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_NEXT_TRACK);
+            Console.WriteLine("NextTrack Pressed");
         }
     }
 }
